624750925ee50c4e938db90ea193035839d4d3f2
jeromeku <jerome.ku@gmail.com>
2025-10-28 16:49:19 +0000

    add install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+# https://docs.modular.com/mojo/manual/gpu/intro-tutorial/
+
+uv pip install mojo \
+  --index-url https://dl.modular.com/public/nightly/python/simple/ \
+  --prerelease allow
+
+#uv sync --upgrade-package mojo
+
+# uv pip install mojo-compiler
+# uv pip install mojo-compiler \
+#   --index-url https://dl.modular.com/public/nightly/python/simple/ \
+#   --prerelease allow
